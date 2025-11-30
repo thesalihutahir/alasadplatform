1c741af9cae8113e07aaf3b8615b6a213ba4b958
Salihu Tahir <realdahirusalihu@gmail.com>
2025-11-30 18:39:05 +0100

    Update page.jsx

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { auth } from "../firebase";
+import { auth, db, storage } from "@/lib/firebase";
 import { createUserWithEmailAndPassword, getIdToken } from "firebase/auth";
 
 export default function SignupPage() {
