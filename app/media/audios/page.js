08905cbbb3f780ec79d0526a9b378fc75cfa5bcc
Salihu Tahir <realdahirusalihu@gmail.com>
2026-03-15 09:15:14 +0100

    Update page.js

--- app/media/audios/page.js
+++ app/media/audios/page.js
@@ -13,7 +13,7 @@ export const metadata = {
     url: 'https://www.alasadfoundation.org/media/audios',
     images: [
       {
-        url: 'https://www.alasadfoundation.org/images/heroes/media-audios-hero.webp',
+        url: 'https://www.alasadfoundation.org/fallback.webp',
       },
     ],
     type: 'website',
@@ -22,7 +22,7 @@ export const metadata = {
     card: 'summary_large_image',
     title: 'Audio Library | Al-Asad Education Foundation',
     images: [
-      'https://www.alasadfoundation.org/images/heroes/media-audios-hero.webp',
+      'https://www.alasadfoundation.org/fallback.webp',
     ],
   },
 };
